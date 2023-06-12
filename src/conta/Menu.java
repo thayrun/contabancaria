d6cc9814d966149d755d7aa6c2dc1e8a1fdf44d9
thayrun <132508300+thayrun@users.noreply.github.com>
2023-06-12 20:23:12 -0300

    +Controllers

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -15,12 +15,12 @@ public class Menu {
 		ContaController contas = new ContaController();
 		Scanner leia = new Scanner(System.in);
 
-		int opcao, numero, agencia, tipo, aniversario;
+		int opcao, numero, agencia, tipo, aniversario, numeroDestino;
 		String titular;
-		float saldo, limite;
+		float saldo, limite, valor;
 		
 		
-		System.out.println("\n Criar contas\n");
+	  //   System.out.println("\n Criar contas\n");
 		
 		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000f, 100.0f);
 		contas.cadastrar(cc1);
@@ -35,6 +35,8 @@ public class Menu {
 		contas.cadastrar(cp2);
 		
 		contas.listarTodas();
+		
+	
 		
 
 		while (true) {
@@ -170,18 +172,50 @@ public class Menu {
 				keyPress();
 				break;
 			case 6:
-				System.out.println("\n Sacar");
+				System.out.println("\n Sacar\n\n");
+				
+				System.out.println("Digite o Numero da conta: ");
+				numero = leia.nextInt();
+				
+				do {
+					System.out.println("Digite o valor do Saque (R$): ");
+					valor = leia.nextFloat();
+				}while(valor <= 0);
+				
+				contas.sacar(numero, valor);
 
 				keyPress();
 				break;
 			case 7:
-				System.out.println("\n Depositar");
+				System.out.println("\n Depositar\n\n");
+				
+				System.out.println("Digite o número da conta: ");
+				numero = leia.nextInt();
+				
+				do {
+					System.out.println("Digite o valor do Depósito (R$): ");
+					valor = leia.nextFloat();
+				}while(valor <= 0);
+				
+				contas.depositar(numero, valor);
 
 				keyPress();
 				break;
 			case 8:
 				System.out.println("\n Transferir");
-
+                   
+				System.out.println("Digite o Numero da Conta de Origem: ");
+				numero = leia.nextInt();
+				System.out.println("Digite o Numero da Conta de Destino: ");
+				numeroDestino = leia.nextInt();
+				
+				do {
+					System.out.println("Digite o Valor da Transferência (R$): ");
+					valor = leia.nextFloat();
+				}while(valor <= 0);
+				
+				contas.transferir(numero, numeroDestino, valor);
+				
 				keyPress();
 				break;
 			default:
--- src/conta/controller/ContaController.java
+++ src/conta/controller/ContaController.java
@@ -60,19 +60,41 @@ public class ContaController implements ContaRepository{
 
 	@Override
 	public void sacar(int numero, float valor) {
-		// TODO Auto-generated method stub
+        var conta = buscarNaCollection(numero);
+        if(conta != null) {
+        	if(conta.sacar(valor) == true)
+        		System.out.println("\nA Conta numero: " + numero + " não foi encontrada!");
+        } else
+				System.out.println("\nA conta numero " + numero + " não foi encontrada");
+		}
 		
-	}
+	
 
 	@Override
 	public void depositar(int numero, float valor) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
 		
+		if(conta != null) {
+			conta.depositar(valor);
+			System.out.println("\nO Depósito na Conta numero: " + numero + " foi efetuado com sucesso!");
+		}else
+		System.out.println("\n Conta numero: " + numero + "não foi encontrada ou a Conta destino não é uma Conta Corrente!");;
 	}
 
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
+		var contaOrigem = buscarNaCollection(numeroOrigem);
+		var contaDestino = buscarNaCollection(numeroDestino);
+		
+		if(contaOrigem != null && contaDestino != null) {
+			if(contaOrigem.sacar(valor) == true) {
+				if(contaOrigem.sacar(valor) == true) {
+					contaDestino.depositar(valor);
+					System.out.println("\nA Transferência foi efetuada com sucesso!");
+				}
+			} else
+				System.out.println("\nA Conta de Origem e/ou Destino não foram encontradas!");
+		}
 		
 	}
 	
